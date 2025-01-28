8539aa8ab2c1221703a47049e2f08748f3d9e048
MahdiHabibiXr <themahdihabibi@gmail.com>
2025-01-28 23:39:56 +0330

    shazam

--- main.py
+++ main.py
@@ -42,8 +42,8 @@ def webhook():
         return jsonify({
             'status': 'success', 
             'message': 'Webhook received and saved',
-            'filename': filename
+            'filename': data['body']
         }), 200
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=False)
+    app.run(host="0.0.0.0", port=5000, debug=True)
